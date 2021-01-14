c7963acf07f5220553b28891f485efc927d6e418
Eduardo Cristiano Thums <eduardocristiano01@gmail.com>
2021-01-14 11:02:36 -0300

    Fix os command injection vulnerability

--- vulnerable_app.py
+++ vulnerable_app.py
@@ -8,8 +8,9 @@ app = Flask(__name__)
 def catch_pokemon():
     pokemon = request.get_json()['pokemon']
 
-    os.system(f'echo {pokemon} >> pokedex.txt')
-    
+    with open('pokedex.txt', 'a') as pokedex:
+        pokedex.write(f'{pokemon}\n')
+
     return jsonify({'message': f'Congratulations, you have added the {pokemon} pokemon into your pokedex!'})
 
 
